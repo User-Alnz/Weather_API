747246b992c174b1be8d6803f1c77444fbb20e9e
User-Alnz <alnav@MacBook-Pro-de-Alexandre.local>
2024-08-08 10:09:30 +0200

    Commit-3

---
+++ Get_API_data.js
@@ -0,0 +1,50 @@
+// https://www.meteomatics.com/en/api/getting-started/
+// https://www.meteomatics.com/en/api/getting-started/
+
+var     position;
+var     latitude;
+var     longitude;
+let     url;
+
+export function ft_Call_Meteomatics()
+{
+    function ft_Get_Weather_Data() 
+    {
+        fetch(url, {
+        method : 'GET',
+        })
+        .then((response) =>{ 
+            if(!response.ok)
+                {
+                    throw new Error('Call is not wroking');
+                }
+            console.log(response.json);
+        })
+        .catch(error=>console.log(error));
+    }
+
+    function ft_GetCurrentLocation(position)
+    {
+
+    // GET current latitude and longitude || Documentation : https://developer.mozilla.org/en-US/docs/Web/API/GeolocationCoordinates
+        if (!navigator.geolocation){
+
+            console.log("Impossible to get current location - Verrify if you allowed access in your browser options");
+
+        }
+        else{
+            navigator.geolocation.getCurrentPosition((position) => {
+
+                latitude = position.coords.latitude.toFixed(2);
+                longitude = position.coords.longitude.toFixed(2);
+                url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,rain,snowfall,wind_speed_10m,wind_direction_10m&timezone=Europe%2FLondon&models=meteofrance_seamless`;
+                //console.log(`latitude is : ${latitude} longitude is : ${longitude}`);
+                //console.log(position.coords.toJSON());
+                ft_Get_Weather_Data();
+            })
+        }
+    }
+        //console.log(url); 
+        //console.log(typeof(latitude));
+        ft_GetCurrentLocation();
+}
---
+++ Main_script.js
@@ -0,0 +1,3 @@
+import {ft_Call_Meteomatics} from "./Get_API_data.js";
+
+ft_Call_Meteomatics();
