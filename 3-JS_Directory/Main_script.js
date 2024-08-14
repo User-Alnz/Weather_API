de7b8b7e07fb284511e4933a4de12d129a1b96a0
User-Alnz <alexandre.anavoizard@gmail.com>
2024-08-14 12:33:55 +0200

    Commit -12 Responsive Done

--- 3-JS_Directory/Main_script.js
+++ 3-JS_Directory/Main_script.js
@@ -1,7 +1,7 @@
 import {ft_Call_Meteomatics_API} from "./Get_API_data.js";
 
-var Data_collection =[];
+var Data_collection = [];
+const display_date_today = document.getElementById("date");
 
 //ft_Call_Meteomatics_API(Data_collection);
-console.log("Data collection is bellow");
 console.log(Data_collection);
