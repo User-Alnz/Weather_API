67787470e2e55faa95323f102fb0beee041f612c
User-Alnz <alexandre.anavoizard@gmail.com>
2024-09-17 09:45:40 +0200

    Commit-30 refacto + cleaning code

--- 3-JS_Directory/weekly_weather_data_functions.js
+++ 3-JS_Directory/weekly_weather_data_functions.js
@@ -1,31 +1,38 @@
+    //------------------------------------------------------------
+        /* Main function  */
+    //------------------------------------------------------------
+
 export function ft_handle_Main_pack_weekly(weekly_item_1, Main_pack_weekly_collection, Hourly_data_collection)
 {   
     var     idx;
     var     nb_days_in_week;
     var     hourly_array_start;
     var     hourly_array_stop;
-    var     off_set_array_to_nxt_24h;
+    var     offset_array_to_nxt_24h;
     
     idx = 0;
     nb_days_in_week = 7;
     hourly_array_start = 1;
     hourly_array_stop = 24;
-    off_set_array_to_nxt_24h = 24;
+    offset_array_to_nxt_24h = 24;
 
-    while(idx < nb_days_in_week)
+    while(idx < nb_days_in_week) //loop into children of Main_pack_weekly_collection <div> and distribute values through function written below. 
     {   
         Main_pack_weekly_collection[idx].getElementsByClassName('day')[0].innerHTML = display_day(idx);
-       
         Main_pack_weekly_collection[idx].getElementsByClassName('day_date')[0].innerHTML = display_date(idx);
+
         Main_pack_weekly_collection[idx].getElementsByClassName('max_temp')[0].innerHTML = `max <strong> +${find_max_value(hourly_array_start, hourly_array_stop, Hourly_data_collection)}°</strong> `;
         Main_pack_weekly_collection[idx].getElementsByClassName('min_temp')[0].innerHTML = `min <strong> +${find_min_value(hourly_array_start, hourly_array_stop, Hourly_data_collection)}°</strong> `;
-        hourly_array_start += off_set_array_to_nxt_24h;
-        hourly_array_stop += off_set_array_to_nxt_24h;
+        hourly_array_start += offset_array_to_nxt_24h;
+        hourly_array_stop += offset_array_to_nxt_24h;
         idx++;
     }
 
 }
-    // Below all function called into ft_handle_Main_pack_weekly 
+    //------------------------------------------------------------
+        /*Below all functions called into ft_handle_Main_pack_weekly */
+    //------------------------------------------------------------
+
     function    display_day(idx)
     {
         const   dayNames_ENG = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
@@ -33,10 +40,11 @@ export function ft_handle_Main_pack_weekly(weekly_item_1, Main_pack_weekly_colle
         var     str;
 
         obj_day = new Date();
-        const   day = obj_day.getDay();
+        const   day = obj_day.getDay(); //method called return value from 0 to 6.
 
-        if(day+idx > 6) // when end of tab is 6. And idx will keep going futher therefor we need to offset idx and reset it to beginnig in code below
-            str=`${dayNames_ENG[(day+idx)%6 -1]}`; // there is 6 possibilities in a row so modulo of 6 will reset result possibilities to one for idx which are gonna be calculated next to 6. then -1 because getDay() got to 0 to 6 max LOL !
+        if(day+idx > 6) //the end of dayNames_ENG is 6. But we need to return to 0, to get days that make up the start of the week. 
+            str=`${dayNames_ENG[(day+idx)%6 -1]}`; //modulo (%6-1) allows an automatic return to 0. like that we can easily catch the next of the week from dayNames_ENG array 
+            //(in a way if day > 6. idx will take lead by finding right day within the array, that's it).
         else 
             str = `${dayNames_ENG[day + idx]}`;
 
@@ -58,16 +66,16 @@ export function ft_handle_Main_pack_weekly(weekly_item_1, Main_pack_weekly_colle
         return(str);
     }
 
-    function    find_max_value(var_or_int_fromArray, var_or_int_toArray, Hourly_data_collection)
+    function    find_max_value(hourly_array_start, hourly_array_stop, Hourly_data_collection)
     {   
         var     idx;
         var     max;
         var     int_value_pointed_in_tab;
 
-        idx = var_or_int_fromArray; // on day is going from idx 0 to 24. So for next day add +24. 
-        max = parseFloat(Hourly_data_collection[idx][1]);
+        idx = hourly_array_start; 
+        max = parseFloat(Hourly_data_collection[idx][1]); //initiate max to float numb for a comparaison below in the loop. Otherwise, it wouldn't work due to 'undefined' value. 
         
-        while(idx++ < var_or_int_toArray)
+        while(idx++ < hourly_array_stop)
         {   
             int_value_pointed_in_tab = parseFloat(Hourly_data_collection[idx][1]); //transf str to float numb
             if (max < int_value_pointed_in_tab)
@@ -77,16 +85,16 @@ export function ft_handle_Main_pack_weekly(weekly_item_1, Main_pack_weekly_colle
         return(max);
     }
 
-    function    find_min_value(var_or_int_fromArray, var_or_int_toArray, Hourly_data_collection)
+    function    find_min_value(hourly_array_start, hourly_array_stop, Hourly_data_collection)
     {
         var     idx;
         var     min;
         var     int_value_pointed_in_tab;
 
-        idx = var_or_int_fromArray;
+        idx = hourly_array_start;
         min = parseFloat(Hourly_data_collection[idx][1]);
         
-        while(idx++ < var_or_int_toArray )
+        while(idx++ < hourly_array_stop)
         {
             int_value_pointed_in_tab = parseFloat(Hourly_data_collection[idx][1]);
 
